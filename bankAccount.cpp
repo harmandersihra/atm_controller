e2f6ca3be81fe9c5f3b784819e53075c12bffaf9
Harmander Sihra <sihraharmander@gmail.com>
2021-05-01 20:07:18 -0700

    Have implementation done. Could do bank better. Need to test

---
+++ bankAccount.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+
+#include "bankAccount.hpp"
+
+// ATM
+bool Atm::InsertCard(BankCard card, int pin)
+{
+    // We first query the bank to see if this user can be logged in
+    AccessToken response = supportedBanks[card.bankId].VerifyUser(card, pin);
+
+    // If we don't get a valid response, then we notify the user and return false
+    if (!response.valid)
+    {
+        std::cout << "Issue logging in, please contact your bank" << std::endl;
+        return false;
+    }
+
+    // If we do get a valid response, we hold on to the token and log the user in
+    token = response;
+    insertedCard = card;
+    return true;
+}
+
+void Atm::RemoveCard()
+{
+    insertedCard = dummyCard;
+}
+
+std::vector<std::string> Atm::ListAccounts()
+{
+    // If user isn't logged in, don't return anything
+    if (insertedCard == dummyCard)
+    {
+        std::cout << "User not logged in. Please insert card" << std::endl;
+        return {};
+    }
+
+    // Query bank for accounts
+    return supportedBanks[insertedCard.bankId].GetAccounts(insertedCard, token);
+}
+
+int Atm::GetBalance(std::string account)
+{
+    // If user isn't logged in, don't return anything
+    if (insertedCard == dummyCard)
+    {
+        std::cout << "User not logged in. Please insert card" << std::endl;
+        return {};
+    }
+
+    // Query bank for balance
+    return supportedBanks[insertedCard.bankId].GetAccountBalance(insertedCard, account, token);
+}
+
+bool Atm::WithdrawMoney(std::string account, int withdrawal)
+{
+    // If user isn't logged in, don't return anything
+    if (insertedCard == dummyCard)
+    {
+        std::cout << "User not logged in. Please insert card" << std::endl;
+        return {};
+    }
+
+    // Check to see if bank allows withdrawal
+    if (supportedBanks[insertedCard.bankId].WithdrawMoney(insertedCard, account, withdrawal, token))
+    {
+        // If it does, then take money out of ATM
+        cashOnHand -= withdrawal;
+        return true;
+    }
+
+    return false;
+}
+
+bool Atm::DepositMoney(std::string account, int deposit)
+{
+    // If user isn't logged in, don't return anything
+    if (insertedCard == dummyCard)
+    {
+        std::cout << "User not logged in. Please insert card" << std::endl;
+        return {};
+    }
+
+    // Check to see if bank allows depositing the money
+    if (supportedBanks[insertedCard.bankId].DepositMoney(insertedCard, account, deposit, token))
+    {
+        // If it does, then add money to the ATM
+        cashOnHand += deposit;
+        return true;
+    }
+
+    return false;
+}
+
+
+// Bank
+/*
+ * This data structure holds all relevant user info and is stored as such:
+ *          unordered map
+ *          /           \
+ *       (key)          (value)
+ * userId (int)   pair <int, vector<string, int>>
+ *                   /         \
+ *       hashed PIN (int)    vector of pairs <string, int>
+ *                              /                  \
+ *                  account name (string)       account balance (int)
+ */
+std::unordered_map<int, std::pair<int, std::vector<std::pair<std::string, int>>>> userInfo;
+
+AccessToken Bank::GenerateToken()
+{
+    return AccessToken(true, time(0));
+}
+
+bool Bank::CheckToken(AccessToken token)
+{
+    return token.valid && (difftime(time(0), token.creationTime) < timeToLive);
+}
+
+bool Bank::CreateAccount(BankCard card, std::string account, int pin)
+{
+    // This function should only be called for new users. If an account already
+    // exists with this user, this will return false
+    if (userInfo.find(card.userId) != userInfo.end())
+    {
+        return false;
+    }
+
+    // Create new account with the hashed pin and empty account
+    userInfo[card.userId] = 
+        std::pair<int, std::vector<std::pair<std::string, int>>>
+            (pinHasher(pin), {std::pair<std::string, int>(account, 0)});
+    
+    return true;
+}
+
+AccessToken Bank::VerifyUser(BankCard card, int pin)
+{
+    // Check to see if the user exists and then if their hashed PIN matches
+    if (userInfo.find(card.userId) == userInfo.end())
+    {
+        std::cout << "User not found. Please create an account first." << std::endl;
+        return invalidResponse;
+    }
+    if (userInfo[card.userId].first != pinHasher(pin))
+    {
+        std::cout << "Incorrect PIN. Please try again." << std::endl;
+        return invalidResponse;
+    }
+
+    return GenerateToken();
+}
+
+std::vector<std::pair<std::string, int>> Bank::GetAccounts(BankCard card, AccessToken token)
+{
+    // Check the token
+    if (!CheckToken(token))
+    {
+        std::cout << "Invalid access token." << std::endl;
+        return {};
+    }
+
+    return userInfo[card.userId].second;
+}
+
+int Bank::GetAccountBalance(BankCard card, std::string account, AccessToken token)
+{
+    // Check the token
+    if (!CheckToken(token))
+    {
+        std::cout << "Invalid access token." << std::endl;
+        return -1;
+    }
+
+    // Go through user's accounts and return balance of selected account
+    for (std::pair<std::string, int> accountEntry : userInfo[card.userId].second)
+    {
+        if (accountEntry.first.compare(account) == 0)
+        {
+            return accountEntry.second;
+        }
+    }
+
+    // If not found, then return error
+    std::cout << "This account does not exist" << std::endl;
+    return 0;
+}
+
+bool Bank::WithdrawMoney(BankCard card, std::string account, int withdrawal, AccessToken token)
+{
+    // Check the token
+    if (!CheckToken(token))
+    {
+        std::cout << "Invalid access token." << std::endl;
+        return false;
+    }
+
+    // Get account balance and see if the withdrawal is valid (not trying to withdraw
+    // more money than is in the account).
+    for (std::pair<std::string, int> accountEntry : userInfo[card.userId].second)
+    {
+        if (accountEntry.first.compare(account) == 0)
+        {
+            // Check to see if they have enough money
+            if (withdrawal > accountEntry.second)
+            {
+                return false;
+            }
+
+            accountEntry.second -= withdrawal;
+            return true;
+        }
+    }
+
+    // If the account isn't found, return false
+    return false;
+}
+
+bool Bank::DepositMoney(BankCard card, std::string account, int deposit, AccessToken token)
+{
+    // Check the token
+    if (!CheckToken(token))
+    {
+        std::cout << "Invalid access token." << std::endl;
+        return -1;
+    }
+
+    // Make sure the account exists and if it does, deposit the money
+    for (std::pair<std::string, int> accountEntry : userInfo[card.userId].second)
+    {
+        if (accountEntry.first.compare(account) == 0)
+        {
+            accountEntry.second += deposit;
+            return true;
+        }
+    }
+
+    // If the account isn't present, we can't do the deposit
+    return false;
+}
--- bankAccount.hpp
+++ bankAccount.hpp
@@ -1,24 +1,53 @@
+#include <functional>
 #include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
 
-class BankCard {
-public:
+struct BankCard
+{
     int userId;
+    int bankId;
 };
 
-class AccessToken {
+bool operator==(const BankCard& card1, const BankCard& card2) {
+    return card1.bankId == card2.bankId &&
+           card1.userId == card2.userId;
+}
+
+class AccessToken
+{
+public:
+    bool valid;
 private:
-    int timeToLive;
     int creationTime;
     friend class Bank;
+
+    AccessToken(bool valid, int creationTime)
+    {
+        valid = valid;
+        creationTime = creationTime;
+    }
 };
 
-class Atm {
+class Atm
+{
 private:
     int cashOnHand;
     BankCard insertedCard;
+    AccessToken token;
+
+    struct BankCard dummyCard = 
+    {
+        -1,
+        -1
+    };
+
+    // I'm not sure how ATMs handle actual communication with banks.
+    // I'd imagine they have some internal table full of numbers or URLs
+    // to query different kinds of banks. I'm simulating this with a map
+    // of supported bank objects
+    std::unordered_map<int, Bank&> supportedBanks;
 
 public:
     bool InsertCard(BankCard card, int pin);
@@ -31,8 +60,23 @@ public:
     bool DepositMoney(std::string account, int deposit);
 };
 
-class Bank {
+class Bank
+{
 private:
+    std::hash<int> pinHasher;
+
+    // Standard TTL for a token will be 5 minutes
+    double timeToLive = 300;
+
+    // This generates a token every time a new user is verified
+    AccessToken GenerateToken();
+
+    // This does a quick verify of the access token passed in
+    bool CheckToken(AccessToken token);
+
+    // Empty access token to send to signify a failed response
+    AccessToken invalidResponse = AccessToken(false, -1);
+
     /*
      * This data structure holds all relevant user info and is stored as such:
      *          unordered map
@@ -47,10 +91,11 @@ private:
     std::unordered_map<int, std::pair<int, std::vector<std::pair<std::string, int>>>> userInfo;
 
 public:
+    bool CreateAccount(BankCard card, std::string account, int pin);
     AccessToken VerifyUser(BankCard card, int pin);
 
-    bool CreateAccount(BankCard card, std::string account, AccessToken token);
-    std::string GetAccounts(BankCard card, AccessToken token);
+    std::vector<std::pair<std::string, int>> GetAccounts(BankCard card, AccessToken token);
+    int GetAccountBalance(BankCard card, std::string account, AccessToken token);
     bool WithdrawMoney(BankCard card, std::string account, int withdrawal, AccessToken token);
     bool DepositMoney(BankCard card, std::string account, int deposit, AccessToken token);
 };
